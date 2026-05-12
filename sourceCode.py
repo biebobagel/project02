31fc5ee5a8c220e4863594bc23fa6424e62998ef
biebobagel <dal574479@utdallas.edu>
2026-05-11 21:04:00 -0500

    cleaned up code with added functions

--- sourceCode.py
+++ sourceCode.py
@@ -1,6 +1,69 @@
 import maya.cmds as cmds
 import maya.OpenMayaUI as omui
 
+def createCamera(centerX, centerY, centerZ, maxSize):
+     # Create a camera and make sure you don't have two
+    if cmds.objExists("turntableCamera"):
+        cmds.delete("turntableCamera")
+
+    camera, cameraShape = cmds.camera(name="turntableCamera")
+    # Position camera based on object size
+    cmds.xform(camera, worldSpace=True, translation=[
+        centerX, centerY + maxSize, centerZ + (maxSize * 3)
+    ])
+    # Aim camera at object
+    cmds.viewPlace(camera, lookAt=[centerX, centerY, centerZ])
+
+    return camera, cameraShape
+
+def createKeyLight(centerX, centerY, centerZ, maxSize):
+    # Create a key light positioned at 45 degrees north of camera
+    keyLight = cmds.directionalLight(name="keyLight")
+    # Return the light's transform node to move it
+    keyLight = cmds.listRelatives(keyLight, parent=True)[0]
+    cmds.xform(keyLight, worldSpace=True, translation=[
+        centerX + (maxSize * 2), centerY + (maxSize * 2),
+        centerZ + (maxSize * 2)
+    ])
+    cmds.xform(keyLight, worldSpace=True, rotation=[-45, 45, 0])
+    # Set to relatively high intensity
+    cmds.setAttr(keyLight + ".intensity", 1.5)
+
+    return keyLight
+
+def createFillLight(centerX, centerY, centerZ, maxSize):
+     # Create/position a fill light positioned at the opposite side
+    fillLight = cmds.directionalLight(name="fillLight")
+    fillLight = cmds.listRelatives(fillLight, parent=True)[0]
+
+    cmds.xform(fillLight, worldSpace=True, translation=[
+        centerX - (maxSize * 2),
+        centerY + (maxSize * 1),
+        centerZ + (maxSize * 2)
+    ])
+
+    # Rotate fill light to object
+    cmds.xform(fillLight, worldSpace=True, rotation=[-20, 135, 0])
+    # Set to low intensity
+    cmds.setAttr(fillLight + ".intensity", 0.5)
+
+    return fillLight
+
+def createRimLight():
+    # Create a rim light
+    rimLight = cmds.directionalLight(name="rimLight")
+    rimLight = cmds.listRelatives(rimLight, parent=True)[0]
+    # Position behind the object at low intensity
+    cmds.xform(rimLight, worldSpace=True, translation=[
+        centerX,
+        centerY + (maxSize * 2),
+        centerZ - (maxSize * 3)
+    ])
+    cmds.xform(rimLight, worldSpace=True, rotation=[-10, 180, 0])
+    cmds.setAttr(rimLight + ".intensity", 1.2)
+
+    return rimLight
+
 def createTurntable(objName):
     # Set time range
     startFrame = 1
@@ -19,8 +82,6 @@ def createTurntable(objName):
 def main():   
     # Select the object in scene
     objectSelection = cmds.ls(selection=True)
-    # If there's no object/selection, display warning/stop 
-
     # Generate a boudning box of object
     objName = objectSelection[0]
     boundingBox = cmds.exactWorldBoundingBox(objName)
@@ -36,61 +97,14 @@ def main():
 
     maxSize = max(width, height, depth)
 
-    # Create a camera and make sure you don't have two
-    if cmds.objExists("turntableCamera"):
-        cmds.delete("turntableCamera")
-
-    camera, cameraShape = cmds.camera(name="turntableCamera")
-    # Position camera based on object size
-    cmds.xform(camera, worldSpace=True, translation=[
-        centerX, centerY + maxSize, centerZ + (maxSize * 3)
-    ])
-    # Aim camera at object
-    cmds.viewPlace(camera, lookAt=[centerX, centerY, centerZ])
-
-    # Create a key light positioned at 45 degrees north of camera
-    keyLight = cmds.directionalLight(name="keyLight")
-    # Return the light's transform node to move it
-    keyLight = cmds.listRelatives(keyLight, parent=True)[0]
-    cmds.xform(keyLight, worldSpace=True, translation=[
-        centerX + (maxSize * 2), centerY + (maxSize * 2),
-        centerZ + (maxSize * 2)
-    ])
-    cmds.xform(keyLight, worldSpace=True, rotation=[-45, 45, 0])
-    # Set to relatively high intensity
-    cmds.setAttr(keyLight + ".intensity", 1.5)
-    
-    # Create/position a fill light positioned at the opposite side
-    fillLight = cmds.directionalLight(name="fillLight")
-    fillLight = cmds.listRelatives(fillLight, parent=True)[0]
-
-    cmds.xform(fillLight, worldSpace=True, translation=[
-        centerX - (maxSize * 2),
-        centerY + (maxSize * 1),
-        centerZ + (maxSize * 2)
-    ])
-
-    # Rotate fill light to object
-    cmds.xform(fillLight, worldSpace=True, rotation=[-20, 135, 0])
-    # Set to low intensity
-    cmds.setAttr(fillLight + ".intensity", 0.5)
-
-    # Create a rim light
-    rimLight = cmds.directionalLight(name="rimLight")
-    rimLight = cmds.listRelatives(rimLight, parent=True)[0]
-    # Position behind the object at low intensity
-    cmds.xform(rimLight, worldSpace=True, translation=[
-        centerX,
-        centerY + (maxSize * 2),
-        centerZ - (maxSize * 3)
-    ])
-    cmds.xform(rimLight, worldSpace=True, rotation=[-10, 180, 0])
-    cmds.setAttr(rimLight + ".intensity", 1.2)
+    createCamera(centerX, centerY, centerZ, maxSize)
+    createKeyLight(centerX, centerY, centerZ, maxSize)
+    createFillLight(centerX, centerY, centerZ, maxSize)
+    createRimLight(centerX, centerY, centerZ, maxSize)
 
     # Create object animation rotating 360 degrees
     # 0-120 frame timeline
     createTurntable(objName)
-
 
     print("Running tool")
 
