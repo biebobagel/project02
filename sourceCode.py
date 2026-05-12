969667ddc99842f5f8651e5289308a2ac101564e
biebobagel <dal574479@utdallas.edu>
2026-05-11 21:40:51 -0500

    ok i think this is all the code..

--- sourceCode.py
+++ sourceCode.py
@@ -17,22 +17,18 @@ def createCamera(centerX, centerY, centerZ, maxSize):
     return camera, cameraShape
 
 def createKeyLight(centerX, centerY, centerZ, maxSize):
-    # Create a key light positioned at 45 degrees north of camera
     keyLight = cmds.directionalLight(name="keyLight")
-    # Return the light's transform node to move it
     keyLight = cmds.listRelatives(keyLight, parent=True)[0]
     cmds.xform(keyLight, worldSpace=True, translation=[
         centerX + (maxSize * 2), centerY + (maxSize * 2),
         centerZ + (maxSize * 2)
     ])
-    cmds.xform(keyLight, worldSpace=True, rotation=[-45, 45, 0])
-    # Set to relatively high intensity
-    cmds.setAttr(keyLight + ".intensity", 1.5)
+    cmds.xform(keyLight, worldSpace=True, rotation=[-35, 60, 0])
+    cmds.setAttr(keyLight + ".intensity", 2.5)
 
     return keyLight
 
 def createFillLight(centerX, centerY, centerZ, maxSize):
-     # Create/position a fill light positioned at the opposite side
     fillLight = cmds.directionalLight(name="fillLight")
     fillLight = cmds.listRelatives(fillLight, parent=True)[0]
 
@@ -42,55 +38,52 @@ def createFillLight(centerX, centerY, centerZ, maxSize):
         centerZ + (maxSize * 2)
     ])
 
-    # Rotate fill light to object
-    cmds.xform(fillLight, worldSpace=True, rotation=[-20, 135, 0])
-    # Set to low intensity
-    cmds.setAttr(fillLight + ".intensity", 0.5)
+    cmds.xform(fillLight, worldSpace=True, rotation=[-10, 135, 0])
+    cmds.setAttr(fillLight + ".intensity", 0.3)
 
     return fillLight
 
 def createRimLight():
-    # Create a rim light
     rimLight = cmds.directionalLight(name="rimLight")
     rimLight = cmds.listRelatives(rimLight, parent=True)[0]
-    # Position behind the object at low intensity
     cmds.xform(rimLight, worldSpace=True, translation=[
         centerX,
         centerY + (maxSize * 2),
         centerZ - (maxSize * 3)
     ])
-    cmds.xform(rimLight, worldSpace=True, rotation=[-10, 180, 0])
-    cmds.setAttr(rimLight + ".intensity", 1.2)
+    cmds.xform(rimLight, worldSpace=True, rotation=[-15, 180, 0])
+    cmds.setAttr(rimLight + ".intensity", 1.8)
 
     return rimLight
 
 def createTurntable(objName):
-    # Set time range
     startFrame = 1
     endFrame = 120
 
-    # Object is at start
+    parent = cmds.listRelatives(objName, parent=True)
+    if parent:
+        objName = parent[0]
+
+    cmds.xform(objName, centerPivots=True)
+    cmds.setAttr(objName + ".rotateY", 0)
+
     cmds.setKeyframe(objName, attribute="rotateY", value=0, time=startFrame)
 
-    # Initiate full rotation
     cmds.setKeyframe(objName, attribute="rotateY", value=360, time=endFrame)
 
-    # Make sure motion is linear and "readable"
     cmds.selectKey(objName, attribute="rotateY")
     cmds.keyTangent(inTangentType="linear", outTangentType="linear")
 
 def main():   
-    # Select the object in scene
     objectSelection = cmds.ls(selection=True)
-    # Generate a boudning box of object
     objName = objectSelection[0]
     boundingBox = cmds.exactWorldBoundingBox(objName)
     xmin, ymin, zmin, xmax, ymax, zmax = boundingBox
-    # Return the center point value(s)
+
     centerX = (xmin + xmax) / 2
     centerY = (ymin + ymax) / 2
     centerZ = (zmin + zmax) / 2
-    # Calculate object size for light placements
+
     width = xmax - xmin
     height = ymax - ymin
     depth = zmax - zmin
@@ -100,10 +93,8 @@ def main():
     createCamera(centerX, centerY, centerZ, maxSize)
     createKeyLight(centerX, centerY, centerZ, maxSize)
     createFillLight(centerX, centerY, centerZ, maxSize)
-    createRimLight(centerX, centerY, centerZ, maxSize)
+    createRimLight()
 
-    # Create object animation rotating 360 degrees
-    # 0-120 frame timeline
     createTurntable(objName)
 
     print("Running tool")
